b7eb675b6ed0bb86ba92efb6c077fdd22309d8bc
Matt <mnfaulk@gmail.com>
2024-08-26 14:07:05 -0400

    Unit testing autoencoder output shapes

--- tests/test_autoencoder.py
+++ tests/test_autoencoder.py
@@ -1,16 +1,17 @@
 import unittest
 
+import torch
 from torch import nn
 
 from neural_networks.symmetric_autoencoder import SymmetricAutoencoder
 
 
 class TestAutoencoder(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.layer_sizes = [784, 256, 64, 16]
         self.autoencoder = SymmetricAutoencoder(self.layer_sizes)
 
-    def test_num_encoder_layers(self):
+    def test_num_encoder_layers(self) -> None:
         """
         The encoder should contain a linear layer between each pair of sizes in the layer sizes list.
         Each linear layer should be followed by a ReLU activation function.
@@ -22,7 +23,7 @@ class TestAutoencoder(unittest.TestCase):
         autoencoder = SymmetricAutoencoder(layer_sizes)
         self.assertEqual(len(autoencoder.encoder), expected_len)
 
-    def test_encoder_layer_types(self):
+    def test_encoder_layer_types(self) -> None:
         """
         Test if the encoder layers are of the correct types.
         """
@@ -34,7 +35,7 @@ class TestAutoencoder(unittest.TestCase):
         self.assertIsInstance(autoencoder.encoder[3], nn.ReLU)
         self.assertIsInstance(autoencoder.encoder[4], nn.Linear)
 
-    def test_encoder_linear_layer_sizes(self):
+    def test_encoder_linear_layer_sizes(self) -> None:
         """
         Test if the encoder linear layers have the correct sizes.
         """
@@ -47,7 +48,7 @@ class TestAutoencoder(unittest.TestCase):
         self.assertEqual(autoencoder.encoder[4].in_features, 64)
         self.assertEqual(autoencoder.encoder[4].out_features, 16)
 
-    def test_num_decoder_layers(self):
+    def test_num_decoder_layers(self) -> None:
         """
         The decoder should contain a linear layer between each pair of sizes in the layer sizes list.
         Each linear layer except the last should be followed by a ReLU activation function.
@@ -61,7 +62,7 @@ class TestAutoencoder(unittest.TestCase):
         autoencoder = SymmetricAutoencoder(layer_sizes)
         self.assertEqual(len(autoencoder.decoder), expected_len)
 
-    def test_decoder_layer_types(self):
+    def test_decoder_layer_types(self) -> None:
         """
         Test if the decoder layers are of the correct types.
         """
@@ -74,7 +75,7 @@ class TestAutoencoder(unittest.TestCase):
         self.assertIsInstance(autoencoder.decoder[4], nn.Linear)
         self.assertIsInstance(autoencoder.decoder[5], nn.Tanh)
 
-    def test_decoder_linear_layer_sizes(self):
+    def test_decoder_linear_layer_sizes(self) -> None:
         """
         Test if the decoder linear layers have the correct sizes.
         """
@@ -86,6 +87,24 @@ class TestAutoencoder(unittest.TestCase):
         self.assertEqual(autoencoder.decoder[2].out_features, 256)
         self.assertEqual(autoencoder.decoder[4].in_features, 256)
         self.assertEqual(autoencoder.decoder[4].out_features, 784)
+
+    def test_forward_pass_output_shapes(self) -> None:
+        """
+        The forward pass should return an output tensor with the same shape as the input tensor,
+        and a latent code tensor with the same shape as the code size.
+        """
+        layer_sizes = [784, 256, 64, 16]
+        # Code size is the last layer size.
+        code_size = layer_sizes[-1]
+        autoencoder = SymmetricAutoencoder(layer_sizes)
+        x = torch.randn(1, 784)
+        output, latent_code = autoencoder.forward(x)
+
+        # The output should have the same shape as the input.
+        self.assertEqual(output.shape, x.shape)
+
+        # The latent code should have the same shape as the code size.
+        self.assertEqual(latent_code.shape, (1, code_size))
 
 
 if __name__ == "__main__":
